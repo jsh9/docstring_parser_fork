8defdbc3897c0ccbfc18e26442347a3e66dc7c41
jsh9 <25124332+jsh9@users.noreply.github.com>
2025-01-12 23:14:44 -0500

    Add size calculation for Docstring objects

--- docstring_parser/tests/test_rest.py
+++ docstring_parser/tests/test_rest.py
@@ -540,3 +540,95 @@ def test_short_rtype() -> None:
     docstring = parse(string)
     rendering_style = RenderingStyle.EXPANDED
     assert compose(docstring, rendering_style=rendering_style) == string
+
+
+@pytest.mark.parametrize(
+    "src, expected_size",
+    [
+        ('', 0),
+        ('Some text', 1),
+        ('Some text\nSome more text', 2),
+        ('Some text\n\nSome more text', 2),
+        ('Some text\n\nSome more text\n\nEven more text', 2),
+        (
+                """
+                Short description.
+
+                This is a longer description.
+
+                Parameters
+                ----------
+                a: int
+                    description
+                b: int
+                    description
+
+                Returns
+                -------
+                int
+                    The return value
+
+                Yields
+                ------
+                str
+                    The yielded value
+
+                Raises
+                ------
+                ValueError
+                    If something goes wrong.
+                TypeError
+                    If something else goes wrong.
+                IOError
+                    If something else goes wrong.
+                """,
+                2,  # because Google style parser can't parse other styles
+        ),
+        (
+                """
+                Short description.
+
+                This is a longer description.
+
+                Args:
+                    a (int): description
+                    b (int): description
+
+                Returns:
+                    int: The return value
+
+                Yields:
+                    str: The yielded value
+
+                Raises:
+                    ValueError: If something goes wrong.
+                    TypeError: If something else goes wrong.
+                    IOError: If something else goes wrong.
+                """,
+                2,  # because ReST style parser can't parse other styles
+        ),
+        (
+                """
+                Short description.
+
+                This is a longer description.
+
+                :param a: description
+                :type a: int
+                :param b: description
+                :type b: int
+                :returns: The return value
+                :rtype: int
+                :yield: The yielded value
+                :rtype: str
+                :raises ValueError: If something goes wrong.
+                :raises TypeError: If something else goes wrong.
+                :raises IOError: If something else goes wrong.
+                """,
+                9,
+        ),
+    ],
+)
+def test_docstring_size(src: str, expected_size: int) -> None:
+    docstring = parse(src)
+    assert docstring.size == expected_size
